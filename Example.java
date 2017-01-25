884f730f8b1559e853f04916c835981d27d6e667
Oxana Gritsenko <ohryt@softserveinc.com>
2017-01-25 10:43:07 +0100

    update

--- Example.java
+++ Example.java
@@ -1,5 +1,7 @@
+
+
 public class Example {
-public static void main(String[] args) {
-System.out.println("something");
-}
+    public static void main(String[] args) {
+        System.out.println("something");
+    }
 }
